a7faf1686bf6e9029dceb329271d8aa713256e48
fragoaaron-lang <fragoaaron@gmail.com>
2026-08-21 23:18:38 +0800

    ci: make db-migrate resilient when dotenv isn't installed

--- server/db-migrate.js
+++ server/db-migrate.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
-require('dotenv').config();
+try {
+  require('dotenv').config();
+} catch (err) {
+  // dotenv may not be installed in some CI images before deps are installed.
+  // Continue without failing; environment variables may be provided by the platform.
+}
+
 const sequelize = require('./config/db');
 
 async function migrate() {
