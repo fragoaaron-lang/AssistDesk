5fbbbd6ac96e13982391d2b54ac5457c89fd3464
jhezzlouise <jhelolicudan16@gmail.com>
2026-09-09 02:07:38 +0800

    Remove specific ticket categories

--- client/src/TicketsPage.js
+++ client/src/TicketsPage.js
@@ -416,10 +416,6 @@ function TicketsPage() {
                 </div>
               )}
               <select className="institutional-select" value={form.category} onChange={(e) => setForm({ ...form, category: e.target.value })}>
-                <option>Broken Door</option>
-                <option>Broken Chair</option>
-                <option>Broken Window</option>
-                <option>Broken PC</option>
                 <option>Hardware</option>
                 <option>Building Maintenance</option>
                 <option>Department Concern</option>
